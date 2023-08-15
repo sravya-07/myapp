0de665153cd4775eae81574f0e8237b42f5f3511
sravya-07 <92778258+sravya-07@users.noreply.github.com>
2023-08-15 15:31:52 +0530

    Update StartBookApplication.java

--- src/main/java/com/mkyong/StartBookApplication.java
+++ src/main/java/com/mkyong/StartBookApplication.java
@@ -24,6 +24,8 @@ public class StartBookApplication {
             repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
             repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
             repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
+            repository.save(new Book("A Book", "Some Author", new BigDecimal("01.10")));
+            
         };
     }
 }
